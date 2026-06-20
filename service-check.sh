aa4ce2ec961a8e95bdf0d677682176a1ee91249b
Phalguni Bavkar <phalgunibavkar@gmail.com>
2026-06-20 22:39:33 +0530

    Delete service-check.sh

--- service-check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
- read -p "enter service name : " SERVICE
- read -p "do you want to check the status ? " STATUS
-
- if [ "$STATUS" == "Y" ]; then
-	 systemctl status $SERVICE
- else 
-	 echo "SKIPPED"
- fi 
